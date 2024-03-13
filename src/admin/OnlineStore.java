abe0e4438a616e0ecd00843267f5fe294904884f
IMU <IMU@DESKTOP-FSRQE0S>
2024-03-13 10:37:23 +0630

    second commit

---
+++ src/admin/OnlineStore.java
@@ -0,0 +1,11 @@
+package admin;
+
+public interface OnlineStore {
+public void addItem();
+public void displayItem();
+public int checkQuantity(String item_name,int quantity);
+public void buyItem();
+public void customerList();
+public void checkReport();
+
+}
